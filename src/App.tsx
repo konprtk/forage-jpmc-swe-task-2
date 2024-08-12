c12a011d78ea7079eab432e99531ac726f4f10f8
Pratik <pratik127001@gmail.com>
2024-08-12 14:00:04 +0530

    Fixed TypeScript issues and updated graph functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,43 @@ import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
 
-/**
- * State declaration for <App />
- */
 interface IState {
   data: ServerRespond[],
 }
 
-/**
- * The parent element of the react app.
- * It renders title, button and Graph react element.
- */
 class App extends Component<{}, IState> {
+  private intervalId: NodeJS.Timeout | null = null;
+
   constructor(props: {}) {
     super(props);
-
     this.state = {
-      // data saves the server responds.
-      // We use this state to parse data down to the child element (Graph) as element property
       data: [],
     };
   }
 
-  /**
-   * Render Graph react component with state.data parse as property data
-   */
   renderGraph() {
     return (<Graph data={this.state.data}/>)
   }
 
-  /**
-   * Get new data from server and update the state with the new data
-   */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    const fetchData = () => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        if (serverResponds.length > 0) {
+          this.setState({ data: [...this.state.data, ...serverResponds] });
+        } else {
+          // Stop the interval if no more data
+          if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+          }
+        }
+      });
+    };
+
+    // Fetch data every 100ms
+    this.intervalId = setInterval(fetchData, 100);
   }
 
-  /**
-   * Render the App react component
-   */
   render() {
     return (
       <div className="App">
@@ -54,12 +48,7 @@ class App extends Component<{}, IState> {
         </header>
         <div className="App-content">
           <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
+            onClick={() => this.getDataFromServer()}>
             Start Streaming Data
           </button>
           <div className="Graph">
@@ -67,7 +56,7 @@ class App extends Component<{}, IState> {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,16 +1,17 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: number;
+  size: number;
 }
+
 /**
  * The datafeed server returns an array of ServerRespond with 2 stocks.
  * We do not have to manipulate the ServerRespond for the purpose of this task.
  */
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string;
+  top_bid: Order;
+  top_ask: Order;
+  timestamp: Date;
 }
 
 class DataStreamer {
@@ -23,15 +24,24 @@ class DataStreamer {
    */
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    request.open('GET', DataStreamer.API_URL, true); // Set to asynchronous
 
     request.onload = () => {
       if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
+        try {
+          const data: ServerRespond[] = JSON.parse(request.responseText);
+          callback(data);
+        } catch (error) {
+          console.error('Failed to parse response JSON:', error);
+        }
       } else {
-        alert ('Request failed');
+        console.error('Request failed with status:', request.status);
       }
-    }
+    };
+
+    request.onerror = () => {
+      console.error('Request failed due to a network error.');
+    };
 
     request.send();
   }
